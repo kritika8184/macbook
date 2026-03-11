48dedf83edbc3c4388a1cf8826317aee9831bce0
kritika8184 <krithika8184@gmail.com>
2026-03-11 15:03:27 +0530

    product viewer section

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ProductViewer = () => {
+    return (
+        <section id="product-viewer">
+            <h2>Take a closer look.</h2>
+            <div className="controls">
+                <p className="info">MacBookPro 16" in Space Black</p>
+                <div className="flex-center gap-5 mt-5">
+                    <div className="color-control">
+                        <div className="bg-neutral-300"/>
+                        <div className="bg-neutral-400"/>
+                    </div>
+
+                    <div className="size-control">
+                        <div><p>14"</p></div>
+                        <div><p>16"</p></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default ProductViewer;
