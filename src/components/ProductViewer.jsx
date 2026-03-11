4baf2f3bcc0120b0612959d038115de6f8a4930f
kritika8184 <krithika8184@gmail.com>
2026-03-11 15:20:00 +0530

    implementation of state via zustand

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
+import useMacBookStore from "../store/index.js";
 
 const ProductViewer = () => {
+    const {color, scale, setColor, setScale} = useMacBookStore();
+
     return (
         <section id="product-viewer">
             <h2>Take a closer look.</h2>
             <div className="controls">
-                <p className="info">MacBookPro 16" in Space Black</p>
+                <p className="info">MacBookPro {scale} in {color}</p>
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
-                        <div className="bg-neutral-300"/>
-                        <div className="bg-neutral-400"/>
+                        <div onClick={()=> setColor('#abd5bd')}
+                             className={clsx('bg-neutral-300', color==='#adb5bd' && 'active')}/>
+                        <div onClick={()=> setColor('#2e2c2e')}
+                             className={clsx('bg-neutral-400', color==='#2e2c2e' && 'active')}/>
                     </div>
 
                     <div className="size-control">
-                        <div><p>14"</p></div>
-                        <div><p>16"</p></div>
+                        <div onClick={()=> setScale(0.06)}
+                             className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')}>
+                            <p>14"</p>
+                        </div>
+                        <div onClick={()=> setScale(0.08)}
+                             className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}>
+                            <p>16"</p>
+                        </div>
                     </div>
                 </div>
             </div>
